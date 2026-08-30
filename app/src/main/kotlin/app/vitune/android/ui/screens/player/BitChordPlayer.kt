42c9ad49fdd57a3bede5b7ccb68e7e24e98bce06
R A J A T <rajatpramanick230@gmail.com>
2026-08-30 23:10:15 +0530

    Remove unused weight import from BitChordPlayer.kt

--- app/src/main/kotlin/app/vitune/android/ui/screens/player/BitChordPlayer.kt
+++ app/src/main/kotlin/app/vitune/android/ui/screens/player/BitChordPlayer.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
